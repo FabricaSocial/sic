f93019a0e20bf3baab8accb3d31b59f2942cdd81
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2014-06-14 09:09:45 -0300

    Unicode das classes adicionado e padronização das classes

--- sic/modelos/pessoa.py
+++ sic/modelos/pessoa.py
@@ -2,88 +2,132 @@ from django import models
 
 
 class CategoriaCNH(models.Model):
-    categoria = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'CategoriaCNH'
 
+    def __unicode__(self):
+        return self.categoria
+
+    categoria = models.CharField(max_length=255)
+
 
 class Nacionalidade(models.Model):
-    descricao = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'Nacionalidade'
 
+    def __unicode__(self):
+        return self.descricao
+
+    descricao = models.CharField(max_length=255)
+
 
 class TipoIdentidade(models.Model):
-    descricao = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'TipoIdentidade'
 
+    def __unicode__(self):
+        return self.descricao
+
+    descricao = models.CharField(max_length=255)
+
 
 class TipoTelefone(models.Model):
-    descricao = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'TipoTelefone'
 
+    def __unicode__(self):
+        return self.descricao
+
+    descricao = models.CharField(max_length=255)
+
 
 class Pais(models.Model):
-    nome = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'Pais'
 
+    def __unicode__(self):
+        return self.nome
+
+    nome = models.CharField(max_length=255)
+
 
 class Etnia(models.Model):
-    descricao = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'Etnia'
 
+    def __unicode__(self):
+        return self.nome
+
+    descricao = models.CharField(max_length=255)
+
 
 class UF(models.Model):
-    pais = models.ForeignKey(Pais)
-    abreviacao = models.CharField(max_length=2)
-    nome = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'UF'
 
+    def __unicode__(self):
+        return self.abreviacao
+
+    pais = models.ForeignKey(Pais)
+    abreviacao = models.CharField(max_length=2)
+    nome = models.CharField(max_length=255)
+
 
 class Cidade(models.Model):
-    uf = models.ForeignKey(UF)
-    nome = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'Cidade'
 
+    def __unicode__(self):
+        return self.nome
+
+    uf = models.ForeignKey(UF)
+    nome = models.CharField(max_length=255)
+
 
 class Naturalidade(models.Model):
-    pais = models.ForeignKey(Pais)
-    cidade = models.ForeignKey(Cidade)
-    uf = models.ForeignKey(UF)
 
     class Meta:
         db_table = 'Naturalidade'
 
+    def __unicode__(self):
+        return self.cidade
+
+    pais = models.ForeignKey(Pais)
+    cidade = models.ForeignKey(Cidade)
+    uf = models.ForeignKey(UF)
+
 
 class Endereco(models.Model):
+
+    class Meta:
+        db_table = 'Endereco'
+
+    def __unicode__(self):
+        return self.endereco
+
     cep = models.BigIntegerField()
     endereco = models.CharField(max_length=255)
     bairro = models.CharField(max_length=255)
     cidade = models.ForeignKey(Cidade)
 
-    class Meta:
-        db_table = 'Endereco'
-
 
 class EstadoCivil(models.Model):
-    descricao = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'EstadoCivil'
+
+    def __unicode__(self):
+      return self.descricao
+
+    descricao = models.CharField(max_length=255)
 
 
 class Pessoa(models.Model):
@@ -109,6 +153,13 @@ class Pessoa(models.Model):
 
 
 class Reservista(models.Model):
+
+    class Meta:
+        db_table = 'Reservista'
+
+    def __unicode__(self):
+        self.certificado
+
     certificado = models.BigIntegerField()
     serie = models.BigIntegerField()
     orgao = models.CharField(max_length=255)
@@ -116,49 +167,69 @@ class Reservista(models.Model):
     unidade_alistamento = models.CharField(max_length=255)
     pessoa = models.ForeignKey(Pessoa)
 
-    class Meta:
-        db_table = 'Reservista'
-
 
 class RegistroGeral(models.Model):
+
+    class Meta:
+        db_table = 'RegistroGeral'
+
+    def __unicode__(self):
+        self.rg
+
     rg = models.BigIntegerField()
     orgao_expedidor = models.CharField(max_length=255)
     data_expedicao = models.DateField()
     pessoa = models.ForeignKey(Pessoa)
     uf = models.ForeignKey(UF)
 
-    class Meta:
-        db_table = 'RegistroGeral'
-
 
 class ServicoMilitar(models.Model):
-    ano = models.IntegerField()
-    pessoa = models.ForeignKey(Pessoa)
-    uf = models.ForeignKey(UF)
 
     class Meta:
         db_table = 'ServicoMilitar'
 
+    def __unicode__(self):
+        self.pessoa
+
+    ano = models.IntegerField()
+    pessoa = models.ForeignKey(Pessoa)
+    uf = models.ForeignKey(UF)
+
 
 class CarteiraTrabalho(models.Model):
-    ctps = models.BigIntegerField()
-    serie = models.IntegerField()
-    pessoa = models.ForeignKey(Pessoa)
 
     class Meta:
         db_table = 'CarteiraTrabalho'
 
+    def __unicode__(self):
+        self.pessoa
+
+    ctps = models.BigIntegerField()
+    serie = models.IntegerField()
+    pessoa = models.ForeignKey(Pessoa)
+
 
 class Filiacao(models.Model):
-    cpf = models.BigIntegerField()
-    nome = models.CharField(max_length=255)
-    pessoa = models.ForeignKey(Pessoa)
 
     class Meta:
         db_table = 'Filiacao'
 
+    def __unicode__(self):
+        self.nome
+
+    cpf = models.BigIntegerField()
+    nome = models.CharField(max_length=255)
+    pessoa = models.ForeignKey(Pessoa)
+
 
 class CNH(models.Model):
+
+    class Meta:
+        db_table = 'CNH'
+
+    def __unicode__(self):
+        self.numero
+
     numero = models.BigIntegerField()
     data_emissao = models.DateField()
     validade = models.DateField()
@@ -166,33 +237,42 @@ class CNH(models.Model):
     categoria_cnh = models.ForeignKey(CategoriaCNH)
     pessoa = models.ForeignKey(Pessoa)
 
-    class Meta:
-        db_table = 'CNH'
-
 
 class Telefone(models.Model):
-    numero = models.BigIntegerField()
-    tipo_telefone = models.ForeignKey(TipoTelefone)
-    pessoa = models.ForeignKey(Pessoa)
 
     class Meta:
         db_table = 'Telefone'
 
+    def __unicode__(self):
+        self.numero
+
+    numero = models.BigIntegerField()
+    tipo_telefone = models.ForeignKey(TipoTelefone)
+    pessoa = models.ForeignKey(Pessoa)
+
 
 class Email(models.Model):
-    endereco = models.CharField(max_length=255)
-    pessoa = models.ForeignKey(Pessoa)
 
     class Meta:
         db_table = 'Email'
 
+    def __unicode__(self):
+        self.endereco
+
+    endereco = models.CharField(max_length=255)
+    pessoa = models.ForeignKey(Pessoa)
+
 
 class TituloEleitor(models.Model):
+
+    class Meta:
+        db_table = 'TituloEleitor'
+
+    def __unicode__(self):
+        self.titulo
+
     titulo = models.BigIntegerField()
     zona = models.IntegerField()
     secao = models.IntegerField()
     pessoa = models.ForeignKey(Pessoa)
     cidade = models.ForeignKey(Cidade)
-
-    class Meta:
-        db_table = 'TituloEleitor'
