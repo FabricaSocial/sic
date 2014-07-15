6e255f2cb4efff6751c61a9678e31f12aa627aca
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2014-07-15 10:06:01 -0300

    Adicionando método de classe para Coordenadoria

--- sic/lista_telefonica/views.py
+++ sic/lista_telefonica/views.py
@@ -3,38 +3,16 @@
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
-from modelos.funcionario import Departamento, Funcionario
+from modelos.funcionario import Departamento
 import json
 
 
 @login_required(login_url='/login')
 def listar_telefones(request):
-    # lista_departamentos = obter_lista_departamentos()
     return render_to_response('lista_telefonica.html',
                               context_instance=RequestContext(request))
 
 
 def obter_lista_departamentos_json(request):
     return render_to_response(json.dumps(Departamento.objects.all()),
-                              content_type="application/json")
-
-
-def obter_lista_funcionarios_por_departamento(request):
-    lista_departamentos = {}
-    lista_funcionarios = {}
-
-    lista_funcionarios_por_departamento = {}
-
-    for departamento in Departamento.objects.all():
-        lista_funcionarios = {}
-        for funcionario in \
-                Funcionario.objects.filter(departamento_id=departamento.id):
-            lista_funcionarios[funcionario.id] = funcionario
-        lista_departamentos[departamento.id] = lista_funcionarios
-
-    return render_to_response(lista_funcionarios_por_departamento)
-
-
-def obter_funcionarios_json(request):
-    return render_to_response(json.dumps(Funcionario.objects.all()),
                               content_type="application/json")
