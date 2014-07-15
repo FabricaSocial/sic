6accb9821dd9fb5407740d89c621e6b6898d2ab0
msfernandes <matheus.souza.fernandes@gmail.com>
2014-07-15 09:27:19 -0300

    Alterando view listar_telefones

--- sic/lista_telefonica/views.py
+++ sic/lista_telefonica/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from modelos.funcionario import Departamento, Funcionario
@@ -9,7 +10,8 @@ import json
 @login_required(login_url='/login')
 def listar_telefones(request):
     # lista_departamentos = obter_lista_departamentos()
-    pass
+    return render_to_response('lista_telefonica.html',
+                              context_instance=RequestContext(request))
 
 
 def obter_lista_departamentos_json(request):
