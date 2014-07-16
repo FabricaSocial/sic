fe7e9882ed921a5454f598d62b0bc6c4ef74115c
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2014-07-16 17:49:44 -0300

    Retirando BASEDIR da foto

--- sic/modelos/pessoa.py
+++ sic/modelos/pessoa.py
@@ -204,8 +204,8 @@ class Pessoa(models.Model):
     data_nascimento = models.DateField(null=True)
     sexo = models.ForeignKey(Sexo, null=True)
     filhos = models.BooleanField(default=False)
-    foto = models.ImageField(upload_to=path.join(BASE_DIR, 'static/img/fotos'),
-                             null=True)
+    foto = models.ImageField(upload_to='static/img/fotos',
+                                null=True)
     etnia = models.ForeignKey(Etnia, null=True)
     tipo_identidade = models.ForeignKey(TipoIdentidade, null=True)
     estado_civil = models.ForeignKey(EstadoCivil, null=True)
