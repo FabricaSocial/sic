f71c69c73ee33ada9e4780f96c97f8e1d2e384ee
msfernandes <matheus.souza.fernandes@gmail.com>
2014-07-18 17:23:14 -0300

    script de configuração de ambiente

---
+++ configurar_ambiente.sh
@@ -0,0 +1,11 @@
+echo "Configuração do ambiente SIC"
+echo "-----------------------------------------"
+echo "Instalando MySQL Server"
+# sudo yum update
+# sudo yum install mysql-server libmysqlclient-devel
+echo ""
+echo "Instalando Python-PIP"
+# sudo yum install python-pip python-devel
+echo ""
+echo "Instalando dependencias do projeto"
+# sudo pip install -r requirements.txt
