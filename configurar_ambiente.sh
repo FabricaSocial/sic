8acdd3f895488723b6e8a0def3e8b996efad21f8
Carlos Henrique Ferreira Oliveira <carloshfoliveira@users.noreply.github.com>
2014-07-21 10:27:30 -0300

    Update configurar_ambiente.sh

--- configurar_ambiente.sh
+++ configurar_ambiente.sh
@@ -5,7 +5,7 @@ echo "Instalando MySQL Server"
 # sudo yum install mysql-server libmysqlclient-devel
 echo ""
 echo "Instalando Python-PIP"
-# sudo yum install python-pip python-devel
+# sudo yum install python-devel
 echo ""
 echo "Instalando dependencias do projeto"
 # sudo pip install -r requirements.txt
