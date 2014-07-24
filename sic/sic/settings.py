a36c254463f940f1d14de24e7deb73f4895f8fc1
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2014-07-24 08:32:31 -0300

    Tentativa de login LDAP?"

--- sic/sic/settings.py
+++ sic/sic/settings.py
@@ -50,9 +50,14 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+AUTHENTICATION_BACKEND = (
+        'django.contrib.auth.middleware.RemoteUserBackend',
+        )
 
 ROOT_URLCONF = 'sic.urls'
 
