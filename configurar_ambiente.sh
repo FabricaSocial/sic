e3fdd53362619bdfa79a9217c93620b5a26bd418
msfernandes <matheus.souza.fernandes@gmail.com>
2014-08-04 11:07:31 -0300

    Atualizando script de configuraçao de ambiente

--- configurar_ambiente.sh
+++ configurar_ambiente.sh
@@ -1,13 +1,16 @@
 echo "Configuração do ambiente SIC"
 echo "-----------------------------------------"
-echo "Instalando MySQL Server"
+echo "Atualizando gerenciador de pacotes"
 yum update
+echo "Instalando MySQL Server"
 yum install -y mysql-server mysql-devel
 echo ""
 echo "Instalando development tools"
 yum group -y install "development tools"
 echo ""
 echo "Instalando Python-PIP"
+wget http://peak.telecommunity.com/dist/ez_setup.py
+python ez_setup.py
 easy_install pip
 echo ""
 echo "Instalando dependencias do projeto"
